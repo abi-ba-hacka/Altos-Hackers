569977d85ff98ff25b3ca9e9ed7b7852bdd78ec5
Dario Adrian Gonzalez <dario@MacBook-Air-de-Dario-2.local>
2017-04-09 15:08:38 -0300

    Agrego QR local

--- resources/views/page-canjear.blade.php
+++ resources/views/page-canjear.blade.php
@@ -4,7 +4,7 @@
 	<main class="biblio canjear">
 		<h2>Canjear regalo</h2>
 		<h3>Presentalo en caja para canjearlo</h3>
-		<img src="http://www.codigos-qr.com/qr/php/qr_img.php?d=http%3A%2F%2Fwww.cervezapatagonia.com.ar%2Frefugios%23sucursales&s=8&e=m" alt="Generador de Códigos QR Codes"/>
+		<img src="{{url('')}}/images/qr.png"/>
 		<h4>18990</h4>
 	</main>
 @stop
